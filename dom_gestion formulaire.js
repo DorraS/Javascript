a5f5c3682cb148526bce683c859d716bb88b920c
saihi <saihidorra86@gmail.com>
2018-02-28 16:05:32 +0100

    dom

--- dom_gestion formulaire.js
+++ dom_gestion formulaire.js
@@ -23,31 +23,42 @@ document.addEventListener("DOMContentLoaded", function(save){
     subscription_login_input.addEventListener("keyup", function (event) {
         // => si saisie entre 4 et 8 caractères, ajouter la class "success" sur le input,
         const saisie = subscription_login_input.value;
-        if(saisie.length >= 4 && saisie.length <= 8){
-            //ajouter la classe "succes" sur le input,
-            subscription_login_input.classList.add('succes');
-            subscription_login_input.classList.remove('error');
-            //console.log("c'est good mate!");
-        } else{
-            // sinon ajouter la classe "error" sur le input
-            subscription_login_input.classList.add('error');
-            subscription_login_input.classList.remove('succes');
-            console.log("c'est pas bon gros!");
+        const list = subscription_login_input.classList;
+        //supprimer toutes les div qui indique des erreurs
+        //1)div qui sont en dessous de l'input:"input ~ div"
+        //2)div qui sont dans form_group:: "div.form_group > div"
+        //je récupère toutes les div d'erreurs
+        const errorDivList = document.querySelectorAll('input ~ div');
+        for (let i= 0; i< errorDivList.length;i++){
+            const div = errorDivList[i];
+            const parent = div.parentNode;
+           parent.removeChild(div);
+           // ou div.parentNode.removeChild(div);
         }
 
+
+        if(saisie.length >= 4 && saisie.length <= 8){
+            //ajouter la classe "succes" sur le input,+ retirer error
+            subscription_login_input.classList.add('succes'); /*list.add('succes')*/
+            subscription_login_input.classList.remove('error');/*list.remove('error')*/
+            //console.log("c'est good mate!");
+        } else {
+            // sinon ajouter la classe "error" sur le input+retirer succes
+            list.add('error');
+            list.remove('remove');
+
+            //ajouter une div qui contient une indication sur l'erreur
+            const errorDiv = document.createElement('div');
+            errorDiv.ClassList.add('error');
+            errorDiv.textContent = 'le login doit contenir entre 4 et 8 caractères';
+
+            //réccupérer le formgroup au dessus du sbscription_login_input
+            const parent = subscription_login_input.parentNOde;
+            //ajouter la div nouvellement créer à ce parent
+            parent.appendChild(errorDiv);
+        }
 
       //  console.log(subscription_login_input.value);
 
     });
 
-
-    // évènement à la soumission du formulaire
-    // => supprimer toutes les div>div dans le formulaire
-    // => vérifier que tous les champs sont renseignés
-    // => vérifier que le login est différent du mot de passe
-    // => vérifier que les mots de passe sont identiques
-    // => vérifier que le login ne contient pas "admin","administrator","superadmin","chuck", "root", "postmaster"
-    // => créer une div juste avant contenant le message d'erreur pour chaque erreur
-
-
-});
---
+++ gestion_formulaire.js
@@ -0,0 +1,49 @@
+/**
+ * gestion du formulaire
+ */
+//esf ce que le fichier js est bien chargé
+console.log('le fichier js bien chargé');
+//code exécuté lorsque leDOM ets chargé
+document.addEventListener("DOMContentLoaded", function (event) {
+    console.log('execution du code js');
+
+    // EVENT LISTENER évènement à la soumission du formulaire
+    const formulaire = document.querySelector('form');
+    formulaire.addEventListener('submit', function (event) {
+        //on supprime le comportement par défaut de l'événement
+        event.preventDefault();
+        console.log('ok');
+
+
+// => supprimer toutes les div>div dans le formulaire
+        const errorDivsToDelete = document.querySelectorAll('div>div');
+        for (let i = 0; i < errorDivsToDelete.length; i++) {
+            const errorDiv = errorDivsToDelete[i];
+            errorDiv.parentNode.removeChild(errorDiv);
+        }
+// => vérifier que tous les champs sont renseignés
+//selecteur par attribut pour récupérer uniquement:
+//type="text" est et type="password"
+        const fieldsRequired = document.querySelectorAll('form input[type=text], form input[type = password]');
+        for (let i = 0; i < fieldsRequired.length; i++) {
+            const field = fieldsRequired[i];
+            const value = field.value;
+            console.log(i + ":" + value, field);
+            if (value.length === 0) {
+                //creer une div qui contient le texte:"ce champs est requis"
+                const errorDiv = document.createElement('div');
+                errorDiv.classList.add('error');
+                errorDiv.textContent = 'ce champs est requis';
+                //ajout de la div error en dessous de l'input
+                field.parentNode.appendChild(errorDiv);
+
+            }
+        }
+
+
+// => vérifier que le login est différent du mot de passe
+// => vérifier que les mots de passe sont identiques
+// => vérifier que le login ne contient pas "admin","administrator","superadmin","chuck", "root", "postmaster"
+// => créer une div juste avant contenant le message d'erreur pour chaque erreur
+    })
+});
